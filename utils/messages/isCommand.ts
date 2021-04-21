08eae2f427556eedf9cac828fa8309c8ab26f427
Garfield is on board <GarfDev.13@gmail.com>
2021-04-21 17:10:29 +0700

    fix: Change isCommand behaviour for edge case

--- utils/messages/isCommand.ts
+++ utils/messages/isCommand.ts
@@ -2,6 +2,7 @@ import getPrefix from './getPrefix';
 
 const isCommand = (message: string): boolean => {
   const prefix = getPrefix();
+  if (prefix === '') return true; // Edge cases
 
   return message.startsWith(prefix);
 };
