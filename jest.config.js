9ae97a6984d2b590188c25babfdf8d12b189a887
Garfield is on board <GarfDev.13@gmail.com>
2021-04-22 21:30:02 +0700

    fix: Add path support for Model directory in Jest

--- jest.config.js
+++ jest.config.js
@@ -24,6 +24,7 @@ module.exports = {
     '^@hooks/(.*?)$': '<rootDir>/hooks/$1',
     '@hooks': '<rootDir>/hooks',
     '^constants/(.*?)$': '<rootDir>/constants/$1',
+    '^models/(.*?)$': '<rootDir>/models/$1',
     errors: '<rootDir>/errors'
   }
   //
