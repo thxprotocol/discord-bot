47180c5ddf5f01bbf95f30aef2fd13771e4e506d
Garfield is on board <GarfDev.13@gmail.com>
2021-04-22 17:58:03 +0700

    feat: Add Guild Owner and Guild Admin command type

--- constants/ListenerType.ts
+++ constants/ListenerType.ts
@@ -1,11 +1,15 @@
 enum ListenerType {
   GENERAL = 'GENERAL',
-  DEVELOPER = 'DEVELOPER'
+  DEVELOPER = 'DEVELOPER',
+  GUILD_OWNER = 'GUILD_OWNER',
+  GUILD_ADMINS = 'GUILD_ADMINS'
 }
 
 /////////////////////////////
 
 export const ListenerTypeLabel = {
+  [ListenerType.GUILD_OWNER]: 'Guild owner only access command',
+  [ListenerType.GUILD_ADMINS]: 'Guild admins only access command',
   [ListenerType.DEVELOPER]: 'Developer only access Commands',
   [ListenerType.GENERAL]: 'General Commands'
 };
