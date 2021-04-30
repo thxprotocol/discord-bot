1ff75d415f4232be16b7b3ba00af506bbfaa2f02
Peter Polman <peter@peterpolman.nl>
2021-04-30 14:16:01 +0200

    Change startup script and remove http server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,10 @@
-export const apps = [
-  {
-    name: 'bot-discord',
-    script: 'dist/server.js',
-    instances: 'max',
-    max_memory_restart: '250M',
-    env_development: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }
-];
+module.exports = {
+    apps: [
+        {
+            name: 'bot-discord',
+            script: 'dist/index.js',
+            instances: 'max',
+            max_memory_restart: '250M',
+        },
+    ],
+};
