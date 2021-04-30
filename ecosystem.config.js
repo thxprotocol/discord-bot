d04611ad8c2fa5b17d17bd950482e49253bb8183
Peter Polman <peter@peterpolman.nl>
2021-04-30 13:28:13 +0200

    Adds a simple http server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 export const apps = [
-    {
-        name: 'bot-discord',
-        script: 'dist/server.js',
-        instances: 'max',
-        max_memory_restart: '250M',
-        env_development: {
-            NODE_ENV: 'development',
-        },
-        env_production: {
-            NODE_ENV: 'production',
-        },
+  {
+    name: 'bot-discord',
+    script: 'dist/server.js',
+    instances: 'max',
+    max_memory_restart: '250M',
+    env_development: {
+      NODE_ENV: 'development'
     },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }
 ];
